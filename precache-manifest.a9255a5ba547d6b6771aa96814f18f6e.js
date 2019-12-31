2efa7b0344817aeee40dcba5e6c5dfdbc91c056b
Yoovin <soo22839@gmail.com>
2019-12-31 22:07:08 +0900

    Updates

---
+++ precache-manifest.a9255a5ba547d6b6771aa96814f18f6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f477112d50bf9892e9772c8c52ff850d",
+    "url": "/SoldierHD/index.html"
+  },
+  {
+    "revision": "d15f8fbd31dc589597f2",
+    "url": "/SoldierHD/static/css/main.fe21a7bc.chunk.css"
+  },
+  {
+    "revision": "914ca66dd988c60b824f",
+    "url": "/SoldierHD/static/js/2.6d42eb66.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/SoldierHD/static/js/2.6d42eb66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d15f8fbd31dc589597f2",
+    "url": "/SoldierHD/static/js/main.5030f2c5.chunk.js"
+  },
+  {
+    "revision": "1f9e9a2310165e893bcd",
+    "url": "/SoldierHD/static/js/runtime-main.c31fccee.js"
+  }
+]);
